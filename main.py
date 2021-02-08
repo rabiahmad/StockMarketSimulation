371d1954cef3aebbe1270558f8484d9de01b2b33
Rabi Ahmad <rabihassanahmad@gmail.com>
2021-02-08 00:29:39 +0000

    fixed bug in calculations

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ if not sys.warnoptions:
 
 
 def run_simulation(ticker, start_date, freq, amt, output):
+    from datetime import datetime
+    from datetime import date
+    import yfinance as yf
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+
+    import sys
+    import warnings
+
+    if not sys.warnoptions:
+        warnings.simplefilter("ignore")
 
     # get data on this ticker
     tickerData = yf.Ticker(ticker)
@@ -20,6 +32,8 @@ def run_simulation(ticker, start_date, freq, amt, output):
     # get the historical prices for this ticker
     cols = ['Open', 'High', 'Low', 'Close']
     tickerDf = tickerData.history(period='1d', start='1975-01-01', end=date.today())[cols]
+
+    df = tickerDf[tickerDf.index >= datetime.strptime(start_date, '%Y-%m-%d')]
 
     def calculate_contributions(start_date, freq, amt, df):
         df['Date'] = df.index
@@ -30,9 +44,7 @@ def run_simulation(ticker, start_date, freq, amt, output):
         df.drop(columns=['Date'], inplace=True)
         df['Cumulative Contributions'] = df['Contribution'].cumsum()
 
-    calculate_contributions(start_date, freq, amt, tickerDf)
-
-    df = tickerDf[tickerDf.index >= datetime.strptime(start_date, '%Y-%m-%d')]
+    calculate_contributions(start_date, freq, amt, df)
 
     # calculating the daily returns
     df['Daily Return'] = df['Close'].pct_change()
@@ -43,9 +55,9 @@ def run_simulation(ticker, start_date, freq, amt, output):
     tot_bal = []
     for row in range(len(df.index)):
         if str(df.iloc[row]['Previous Return'].item()) == "nan":
-            tot_bal.append(df.iloc[row]['Contribution'].item())
+            tot_bal.append(df.iloc[row]['Cumulative Contributions'].item())
         else:
-            bal = tot_bal[row - 1] * (1 + df.iloc[row]['Previous Return'].item()) + df.iloc[row]['Contribution'].item()
+            bal = tot_bal[row - 1] * (1 + df.iloc[row - 1]['Daily Return'].item()) + df.iloc[row]['Contribution'].item()
             tot_bal.append(bal)
 
     TotalBalanceDF = pd.DataFrame({"Total Balance": tot_bal})
